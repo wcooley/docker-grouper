2482b41759ec382fcd4c16b9123d1d85c1f4b53c
villadalmine <rino@atadata.com>
2016-09-07 12:07:04 -0300

    Added more log

--- container_files/bin/check.sh
+++ container_files/bin/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-log="/tmp/grouper.log"
+log="/tmp/grouper-check.log"
 
 echo "Installing schema" >> $log
 cd /opt/grouper/2.3.0/grouper.apiBinary-2.3.0 && GROUPER_HOME=/opt/grouper/2.3.0/grouper.apiBinary-2.3.0 bin/gsh.sh -registry -drop -runscript -noprompt >> $log
