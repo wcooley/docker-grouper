b600f3e5dd8b1da00133856366424fd0e657cb39
villadalmine <rino@atadata.com>
2016-09-08 11:00:17 -0300

    Added noprompt in order to finish the last check

--- container_files/bin/check.sh
+++ container_files/bin/check.sh
@@ -12,4 +12,4 @@ echo "Adding Quickstart data" >> $log
 cd /opt/grouper/2.3.0/grouper.apiBinary-2.3.0 && GROUPER_HOME=/opt/grouper/2.3.0/grouper.apiBinary-2.3.0 bin/gsh.sh -xmlimportold GrouperSystem /opt/grouper/2.3.0/quickstart.xml -noprompt >> $log
 
 echo "Checking" >> $log
-cd /opt/grouper/2.3.0/grouper.apiBinary-2.3.0 && GROUPER_HOME=/opt/grouper/2.3.0/grouper.apiBinary-2.3.0 bin/gsh.sh -registry -check >> $log
+cd /opt/grouper/2.3.0/grouper.apiBinary-2.3.0 && GROUPER_HOME=/opt/grouper/2.3.0/grouper.apiBinary-2.3.0 bin/gsh.sh -registry -check -noprompt >> $log
