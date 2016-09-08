c1a61404a8fec051b00b6720987c73a4f75917a1
villadalmine <rino@atadata.com>
2016-09-08 12:25:22 -0300

    Added more logic to scripts

--- container_files/bin/apache-grouper.sh
+++ container_files/bin/apache-grouper.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
+log=/tmp/apache-supervisor.log
+date >> $log
 ps auxww |grep -iq start.sh
 statusstart=$?
 
 while [ "$statusstart" != 0 ]; do
 ps auxww |grep -iq start.sh
 statusstart=$?
-echo "First start configuration is in process, please wait"
+echo "First start configuration is in process, please wait" >> $log
 done
-echo "Starting Apache"
+echo "Starting Apache" >> $log
+date >> $log
 /usr/local/bin/httpd-shib-foreground
--- container_files/bin/tomcat-grouper.sh
+++ container_files/bin/tomcat-grouper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+log=/tmp/tomcat-apache-supervisor.log
+date >> $log
 ps auxww |grep -iq start.sh
 statusstart=$?
 ps auxww | grep -iq bin/httpd-shib-foreground
@@ -8,11 +9,12 @@ statusapache=$?
 while [ "$statusstart" != 0 ] && [ "$statusapache" == "0" ]; do
 ps auxww |grep -iq start.sh
 statusstart=$?
-echo "First start configuration is in process, please wait"
+echo "First start configuration is in process, please wait" >> $log
 ps auxww | grep -iq bin/httpd-shib-foreground
 statusapache=$?
-echo "Apache is not running, please wait"
+echo "Apache is not running, please wait" >> $log
 done
-echo "Starting Tomcat"
+echo "Starting Tomcat" >> $log
+date >> $log
 /opt/grouper/2.3.0/apache-tomcat-6.0.35/bin/catalina.sh run
 
