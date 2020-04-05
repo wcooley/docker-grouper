42754a01c0487cf687ace8654157292763c7483c
Christopher Hubing <chubing@internet2.edu>
2020-04-04 20:39:16 -0400

    Update entrypoint.sh

--- container_files/usr-local-bin/entrypoint.sh
+++ container_files/usr-local-bin/entrypoint.sh
@@ -5,8 +5,10 @@ prepConf
 
 if [ "$#" -eq 0 ]; 
   then
+   echo no component set to run
    finishPrep
    exec /usr/bin/supervisord -c /opt/tier-support/supervisord.conf
 else
+  echo executing $@
   exec "$@"
 fi
