2006073c4152748ba73c2f2371da551e9b8be78a
Christopher Hubing <chubing@internet2.edu>
2020-04-08 13:12:06 -0400

    adding CATALINA_OPTS

--- container_files/tomee/bin/setenv.sh
+++ container_files/tomee/bin/setenv.sh
@@ -1,3 +1,4 @@
 CLASSPATH=/opt/tomee/bin/*
-JAVA_OPTS="-Dlog4j.configurationFile=/opt/tomee/conf/log4j2.xml -DENV=$ENV -DUSERTOKEN=$USERTOKEN"
+#JAVA_OPTS="-Dlog4j.configurationFile=/opt/tomee/conf/log4j2.xml -DENV=$ENV -DUSERTOKEN=$USERTOKEN"
+CATALINA_OPTS="-Xmx$GROUPER_MAX_MEMORY -XX:+UseG1GC -XX:+UseStringDeduplication -Dlog4j.configurationFile=/opt/tomee/conf/log4j2.xml -DENV=$ENV -DUSERTOKEN=$USERTOKEN $GROUPER_EXTRA_CATALINA_OPTS"
 LOGGING_MANAGER=-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
