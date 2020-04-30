89000fb9b350cd41083c3904f57a0ccbcbef2d57
mchyzer <mchyzer@macbook-pro-103.lan>
2020-04-30 15:24:57 -0400

    GRP-2730: grouper not showing accented characters

--- container_files/tomee/bin/setenv.sh
+++ container_files/tomee/bin/setenv.sh
@@ -1,4 +1,4 @@
 CLASSPATH=/opt/tomee/bin/*
 #JAVA_OPTS="-Dlog4j.configurationFile=/opt/tomee/conf/log4j2.xml -DENV=$ENV -DUSERTOKEN=$USERTOKEN"
-CATALINA_OPTS="-Xmx$GROUPER_MAX_MEMORY -XX:+UseG1GC -XX:+UseStringDeduplication -Dlog4j.configurationFile=/opt/tomee/conf/log4j2.xml -DENV=$ENV -DUSERTOKEN=$USERTOKEN $GROUPER_EXTRA_CATALINA_OPTS"
+CATALINA_OPTS="-Xmx$GROUPER_MAX_MEMORY -XX:+UseG1GC -XX:+UseStringDeduplication -Dlog4j.configurationFile=/opt/tomee/conf/log4j2.xml -DENV=$ENV -DUSERTOKEN=$USERTOKEN -Dfile.encoding=UTF-8 $GROUPER_EXTRA_CATALINA_OPTS"
 LOGGING_MANAGER=-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager
