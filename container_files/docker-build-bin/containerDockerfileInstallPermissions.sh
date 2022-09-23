6ec30e767bf0be6a35688e24132e970ccd81eb3a
mchyzer <mchyzer@isc.upenn.edu>
2022-09-23 17:24:20 -0400

    GRP-4384: container should not change ownership or permissions on /opt/grouper/slashRoot

--- container_files/docker-build-bin/containerDockerfileInstallPermissions.sh
+++ container_files/docker-build-bin/containerDockerfileInstallPermissions.sh
@@ -8,67 +8,70 @@ fi
 user=$1
 group=$2
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts ! -user $user -print | wc -l)
+# this needs to exist
+mkdir -p /opt/tier
+
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -user $user -print | wc -l)
 if [ $lines -ne 0 ]; then
-  chown $user:$group $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts ! -user $user -print)
+  chown $user:$group $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -user $user -print)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chown $user:$group \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts ! -user $user -print), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chown $user:$group \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -user $user -print), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts ! -group $group -print | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -group $group -print | wc -l)
 if [ $lines -ne 0 ]; then
-  chown $user:$group $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts ! -group $group -print)
+  chown $user:$group $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -group $group -print)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chown $user:$group \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts ! -group $group -print), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chown $user:$group \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -group $group -print), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ -type d ! -perm -g+rws | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ -type d -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+rwxs | wc -l)
 if [ $lines -ne 0 ]; then
-  chmod g+rws $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ -type d ! -perm -g+rws)
+  chmod g+rwxs $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ -type d -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+rwxs)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod g+rws \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ -type d ! -perm -g+rws ), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod g+rwxs \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ -type d -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+rwxs ), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -type f ! -perm -g+rw | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -type f -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+rw | wc -l)
 if [ $lines -ne 0 ]; then
-  chmod g+rw $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -type f ! -perm -g+rw)
+  chmod g+rw $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -type f -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+rw)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod g+rw \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -type f ! -perm -g+rw ), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod g+rw \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -type f -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+rw ), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -perm -o+w | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -perm -o+w -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot | wc -l)
 if [ $lines -ne 0 ]; then
-  chmod o-w $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -perm -o+w)
+  chmod o-w $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -perm -o+w -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod o-w \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -perm -o+w ), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod o-w \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /usr/local/bin /etc/httpd/conf.d/ /usr/lib/jvm/java/jre/lib/security/cacerts -perm -o+w -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -g+x | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -g+x -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot | wc -l)
 if [ $lines -ne 0 ]; then
-  chmod +x $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -g+x)
+  chmod +x $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+x)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name \"*.sh\" ! -perm -g+x), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name \"*.sh\" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -g+x), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -u+x | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -u+x | wc -l)
 if [ $lines -ne 0 ]; then
-  chmod +x $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -u+x)
+  chmod +x $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -u+x)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name \"*.sh\" ! -perm -u+x), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name \"*.sh\" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -u+x), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
-lines=$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -o+x | wc -l)
+lines=$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -o+x | wc -l)
 if [ $lines -ne 0 ]; then
-  chmod +x $(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" ! -perm -o+x)
+  chmod +x $(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name "*.sh" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -o+x)
   returnCode=$?
-  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name \"*.sh\" ! -perm -o+x), result: $returnCode"
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /home/$user /opt/container_files/ /opt/grouper/ /opt/tier/ /opt/tier-support/ /opt/tomee/ /etc/httpd/conf/ /home/tomcat/ /etc/httpd/conf.d/ -type f -name \"*.sh\" -not -path /opt/grouper/slashRoot/* -not -path /opt/grouper/slashRoot ! -perm -o+x), result: $returnCode"
   if [ $returnCode != 0 ]; then exit $returnCode; fi
 fi
 
