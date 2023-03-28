4bd93403faa2ed40e853925416457a7a2899a99b
mchyzer <mchyzer@isc.upenn.edu>
2023-03-28 03:17:38 -0400

    4.1.1

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ docker run -d -p 443:443 --name my-grouper \
        -e GROUPER_UI_GROUPER_AUTH=true \
        -e GROUPER_SELF_SIGNED_CERT=true \
        -e GROUPER_RUN_SHIB_SP=false \
-       -e GROUPER_AUTO_DDL_UPTOVERSION='v2.6.*' \
+       -e GROUPER_AUTO_DDL_UPTOVERSION='v4.0.*' \
        -e GROUPER_UI_CONFIGURATION_EDITOR_SOURCEIPADDRESSES='0.0.0.0/0' \
        -e GROUPERSYSTEM_QUICKSTART_PASS=pass \
        -e GROUPER_UI=true \
