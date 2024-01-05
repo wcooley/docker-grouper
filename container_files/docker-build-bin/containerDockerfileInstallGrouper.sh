2881fd816200f9392ea4fdc4c19bc708341810d8
Chad Redman <credman@unicon.net>
2024-01-05 15:42:51 -0500

    GRP-5135: Rewrite container installer as a script (5.x)
    * Rewrite containerDockerfileInstallGrouper.sh to simulate what the installer does
    * multi-stage build
    * Removed dos2unix (should not be needed)
    * Removed /opt/container_files from final image (containerDockerfileInstallPermissions.sh moved to /usr/local/bin)
    * Moved localhost.pem and localhost.key to /opt/grouper/certs (i.e. one level up)

--- container_files/docker-build-bin/containerDockerfileInstallGrouper.sh
+++ container_files/docker-build-bin/containerDockerfileInstallGrouper.sh
@@ -1,47 +1,218 @@
 #!/bin/bash
 
-# $1 ARG JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto
-# $2 ARG GROUPER_VERSION=2.6.14
-JAVA_HOME=$1
-GROUPER_VERSION=$2
+# These need to be set correctly
+# ARG JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto
+# ARG GROUPER_VERSION=2.x.x
 
-mv /opt/container_files/tier-support /opt
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) mv /opt/container_files/tier-support /opt, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
 
-mkdir -p /opt/grouper/$GROUPER_VERSION
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) , result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
+printLog() {
+    echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) $1"
+}
 
-wget -q -O /opt/grouper/$GROUPER_VERSION/grouperInstaller.jar https://oss.sonatype.org/service/local/repositories/releases/content/edu/internet2/middleware/grouper/grouper-installer/$GROUPER_VERSION/grouper-installer-$GROUPER_VERSION.jar
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) wget -q -O /opt/grouper/$GROUPER_VERSION/grouperInstaller.jar https://oss.sonatype.org/service/local/repositories/releases/content/edu/internet2/middleware/grouper/grouper-installer/$GROUPER_VERSION/grouper-installer-$GROUPER_VERSION.jar, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
+runCmd() {
+    cmd=$1
+    $cmd
+    returnCode=$?
+    printLog "$cmd, result: $returnCode"
+    if [ $returnCode != 0 ]; then exit $returnCode; fi
+}
 
-mv /opt/container_files/grouper.installer.properties /opt/grouper/$GROUPER_VERSION
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) mv /opt/container_files/grouper.installer.properties /opt/grouper/$GROUPER_VERSION, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
 
-# Temporary morphString file used for building, not used in production
-mv /opt/container_files/morphString.properties /opt/grouper/$GROUPER_VERSION
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) mv /opt/container_files/morphString.properties /opt/grouper/$GROUPER_VERSION, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
+if [[ "$GROUPER_VERSION" =~ -SNAPSHOT$ ]]; then
+    isSnapshot=1
+    grouperSourceBranch=GROUPER_$(echo $GROUPER_VERSION | cut -c1)_BRANCH
+else
+    isSnapshot=0
+    grouperSourceBranch=GROUPER_RELEASE_$GROUPER_VERSION
+fi
 
-cd /opt/grouper/$GROUPER_VERSION/
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) cd /opt/grouper/$GROUPER_VERSION/, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
+printLog "detected isSnapshot=$isSnapshot and grouperSourceBranch=$grouperSourceBranch"
 
-$JAVA_HOME/bin/java -cp :grouperInstaller.jar edu.internet2.middleware.grouperInstaller.GrouperInstaller
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) $JAVA_HOME/bin/java -cp :grouperInstaller.jar edu.internet2.middleware.grouperInstaller.GrouperInstaller, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
+mountDir=/tmp/container_files
+tarballDir=/tmp/tarballs
+containerDir=/tmp/stage/grouper/$GROUPER_VERSION/container
+containerTomcatDir=$containerDir/tomcat
+webAppDir=$containerDir/grouper/grouperWebapp
 
-rm -rf /root/.m2
-returnCode=$?
-echo "grouperDockerfile; INFO: (containerDockerfileInstallGrouper.sh) rm -rf /root/.m2, result: $returnCode"
-if [ $returnCode != 0 ]; then exit $returnCode; fi
+i2ServerBase=https://software.internet2.edu/grouper
+mvnVersion=3.6.3
+tomcatVersion=9.0.87  # 8.5.90
+MVN=$tarballDir/apache-maven-$mvnVersion/bin/mvn
+
+# cp command is aliased to interactive which affects automated scripts
+unalias cp
+unalias rm
+
+printLog "GROUPER_VERSION=$GROUPER_VERSION"
+printLog "isSnapshot=$isSnapshot"
+printLog "grouperSourceBranch=$grouperSourceBranch"
+printLog "mvnVersion=$mvnVersion"
+printLog "tomcatVersion=$tomcatVersion"
+
+
+# Create the tarballs directory
+runCmd "mkdir -p $tarballDir"
+
+# Download and unpack Maven
+runCmd "curl -L -k $i2ServerBase/downloads/tools/apache-maven-$mvnVersion-bin.tar.gz --output $tarballDir/apache-maven-$mvnVersion-bin.tar.gz"
+runCmd "tar xzf $tarballDir/apache-maven-$mvnVersion-bin.tar.gz -C $tarballDir"
+
+# Download and unpack Tomcat
+runCmd "curl -L -k $i2ServerBase/downloads/tools/apache-tomcat-$tomcatVersion.tar.gz   --output $tarballDir/apache-tomcat-$tomcatVersion.tar.gz"
+runCmd "tar xzf $tarballDir/apache-tomcat-$tomcatVersion.tar.gz -C $tarballDir"
+
+# Download grouper source
+if [ $isSnapshot != 1 ]; then
+    runCmd "curl -L -k https://github.com/Internet2/grouper/archive/$grouperSourceBranch.tar.gz --output $tarballDir/$grouperSourceBranch.tar.gz"
+    runCmd "tar xzf $tarballDir/$grouperSourceBranch.tar.gz -C $tarballDir"
+
+    grouperReleaseDir=$tarballDir/grouper-$grouperSourceBranch
+else
+    runCmd "yum install -y git"
+    runCmd "rm -rf $tarballDir/grouper-$GROUPER_VERSION && git clone --depth 1 --branch $grouperSourceBranch https://github.com/Internet2/grouper.git $tarballDir/grouper-$GROUPER_VERSION"
+
+    grouperReleaseDir=$tarballDir/grouper-$GROUPER_VERSION
+fi
+
+# set up staging directories
+runCmd "mkdir -p $containerDir $containerTomcatDir $webAppDir"
+
+# copy webapp
+runCmd "rsync -avzpl $grouperReleaseDir/grouper-ui/webapp/* $webAppDir"
+
+# create staging subfolders
+#    skipping in v5: $webAppDir/WEB-INF/modules $webAppDir/WEB-INF/services
+runCmd "mkdir -p $webAppDir/WEB-INF $webAppDir/conf $webAppDir/WEB-INF/lib $webAppDir/WEB-INF/libUiAndDaemon $webAppDir/WEB-INF/libWs $webAppDir/WEB-INF/classes/grouperText $webAppDir/WEB-INF/bin"
+
+
+# copy dependencies - API
+runCmd "$MVN -f $grouperReleaseDir/grouper-container/grouper-api-container -DincludeScope=runtime -Dgrouper.version=$GROUPER_VERSION dependency:copy-dependencies"
+runCmd "cp -p $grouperReleaseDir/grouper-container/grouper-api-container/target/dependency/*.jar $webAppDir/WEB-INF/lib"
+
+# copy dependencies - UI
+runCmd "$MVN -f $grouperReleaseDir/grouper-container/grouper-uiDaemon-container -DincludeScope=runtime -Dgrouper.version=$GROUPER_VERSION dependency:copy-dependencies"
+runCmd "cp -p $grouperReleaseDir/grouper-container/grouper-uiDaemon-container/target/dependency/*.jar $webAppDir/WEB-INF/libUiAndDaemon"
+
+# copy dependencies - WS
+runCmd "$MVN -f $grouperReleaseDir/grouper-container/grouper-ws-container -DincludeScope=runtime -Dgrouper.version=$GROUPER_VERSION dependency:copy-dependencies"
+runCmd "cp -p $grouperReleaseDir/grouper-container/grouper-ws-container/target/dependency/*.jar $webAppDir/WEB-INF/libWs"
+
+# copy app conf/* to classes
+for dir in grouper grouper-ws/grouper-ws grouper-ui grouper-misc/grouperClient; do
+    runCmd "rsync -avzpl $grouperReleaseDir/$dir/conf/* $webAppDir/WEB-INF/classes/"
+done
+
+# copy staging conf to classes (mainly log4j)
+runCmd "rsync -avzpl $mountDir/grouperWebapp/WEB-INF/classes/* $webAppDir/WEB-INF/classes/"
+
+# copy example files to classes renaming to remove .examples
+for file in $grouperReleaseDir/grouper/misc/*example.properties; do
+    if [[ "$file" =~ grouper.text.en.us.example.properties$ ]]; then
+        newFile=$webAppDir/WEB-INF/classes/grouperText/grouper.text.en.us.properties
+    else
+        newFile="$webAppDir/WEB-INF/classes/${file##*/}"
+        newFile=${newFile//.example./.}
+    fi
+    runCmd "cp -p $file $newFile"
+done
+
+# copy WS conf subdirectories to WEB-INF/conf
+# (SOAP gone in v5) for dir in "webapp/WEB-INF/modules webapp/WEB-INF/services conf"; do
+#for dir in "conf"; do
+#    runCmd "rsync -avzpl $grouperReleaseDir/grouper-ws/grouper-ws/$dir $webAppDir/dir/"
+#done
+
+# copy bin
+runCmd "rsync -avzpl $grouperReleaseDir/grouper/bin/* $webAppDir/WEB-INF/bin/"
+
+# make gsh executable
+runCmd "chmod +x $webAppDir/WEB-INF/bin/gsh.sh"
+
+# download grouper jars
+for app in grouper grouperClient; do
+    runCmd "curl -L -k https://oss.sonatype.org/service/local/repositories/releases/content/edu/internet2/middleware/grouper/$app/$GROUPER_VERSION/$app-$GROUPER_VERSION.jar --output $webAppDir/WEB-INF/lib/$app-$GROUPER_VERSION.jar"
+done
+
+# Removed in v5: grouper-pspng  grouper-box grouper-duo grouper-azure-provisioner
+for app in grouper-ui; do
+    runCmd "curl -L -k https://oss.sonatype.org/service/local/repositories/releases/content/edu/internet2/middleware/grouper/$app/$GROUPER_VERSION/$app-$GROUPER_VERSION.jar --output $webAppDir/WEB-INF/libUiAndDaemon/$app-$GROUPER_VERSION.jar"
+done
+
+for app in grouper-ws; do
+    runCmd "curl -L -k https://oss.sonatype.org/service/local/repositories/releases/content/edu/internet2/middleware/grouper/$app/$GROUPER_VERSION/$app-$GROUPER_VERSION.jar --output $webAppDir/WEB-INF/libWs/$app-$GROUPER_VERSION.jar"
+done
+
+
+# delete slf4j related jars from all the lib dirs
+## not needed?
+
+
+# Remove older versions of jars
+## TODO
+
+# Delete duplicate copies of jars in other libs
+for destination_dir in $webAppDir/WEB-INF/libUiAndDaemon $webAppDir/WEB-INF/libWs; do
+    for file in $webAppDir/WEB-INF/lib/*; do
+        # Check if the file exists in the destination directory
+        basefile="$destination_dir/$(basename "$file")"
+        if [ -e "$basefile" ]; then
+            # Delete the file in the destination directory
+            rm "$basefile"
+            echo "Deleted $basefile in $destination_dir"
+        fi
+    done
+done
+printLog "Removed files from libUiAndDaemon and libWs"
+
+
+# copy apache-tomcat-x.y.z to tomcat
+runCmd "rsync -avzpl $tarballDir/apache-tomcat-$tomcatVersion/* $containerTomcatDir"
+
+# make tomcat files executable
+runCmd "chmod +x $containerTomcatDir/bin/*.sh"
+
+
+
+# install grouper files for tomcat
+runCmd "rsync -avzpl $mountDir/tomcat/* $containerTomcatDir"
+
+
+# copy tier-support
+runCmd "rsync -avzpl $mountDir/tier-support $containerDir"
+
+
+# Make logs dir
+runCmd "mkdir -p $containerDir/grouper/logs"
+runCmd "chown -R tomcat.root $containerDir/grouper/logs"
+runCmd "chmod -R g+rwxs $containerDir/grouper/logs/"
+
+
+# Clean up tomcat
+runCmd "rm -rf $containerTomcatDir/webapps/ROOT $containerTomcatDir/webapps/examples $containerTomcatDir/webapps/docs/ $containerTomcatDir/webapps/host-manager/ $containerTomcatDir/webapps/manager/ $containerTomcatDir/logs/* $containerTomcatDir/temp/* $containerTomcatDir/work/* $containerTomcatDir/conf/logging.properties"
+runCmd "rm -f $containerTomcatDir/bin/log4j-*"
+runCmd "rm -f $containerTomcatDir/lib/slf4j-*"
+
+runCmd "mkdir -p $containerTomcatDir/conf/Catalina/localhost/"
+runCmd "mkdir -p $containerTomcatDir/work/Catalina/localhost/"
+
+runCmd "cp -p $mountDir/tier-support/log4j_fix/tomcatBin/log4j-* $containerTomcatDir/bin/"
+runCmd "cp -p $mountDir/tier-support/log4j_fix/tomcatLib/slf4j-* $containerTomcatDir/lib/"
+
+runCmd "rm -f $webAppDir/WEB-INF/lib/slf4j-api-*"
+
+runCmd "cp -p $mountDir/tier-support/log4j_fix/webinfLib/* $webAppDir/WEB-INF/lib/"
+
+runCmd "touch $containerDir/grouper/grouperEnv.sh"
+
+
+runCmd "mkdir -p $containerDir/grouper/certs/client $containerDir/grouper/certs/keys $containerDir/grouper/certs/anchors"
+
+# put localhost and test certs outside where the bootstrap would pick them up
+runCmd "cp -p $mountDir/certs/localhost.*   $containerDir/grouper/certs/"
+
+
+runCmd "mkdir -p $containerDir/tier-support/originalFiles"
+runCmd "cp $webAppDir/WEB-INF/classes/log4j2.xml $containerDir/tier-support/originalFiles"
+runCmd "cp $containerTomcatDir/conf/server.xml $containerDir/tier-support/originalFiles"
+runCmd "cp $containerTomcatDir/conf/Catalina/localhost/grouper.xml $containerDir/tier-support/originalFiles"
+runCmd "cp $webAppDir/WEB-INF/web.xml $containerDir/tier-support/originalFiles"
---
+++ container_files/usr-local-bin/containerDockerfileInstallPermissions.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo 'pass in user and group, e.g. /usr/local/bin/containerDockerfileInstallPermissions.sh tomcat root'
+    exit 1
+fi
+
+user=$1
+group=$2
+
+# this needs to exist
+mkdir -p /opt/tier
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o ! -user $user -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chown $user:$group $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o ! -user $user -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chown $user:$group \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o ! -user $user -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o ! -group $group -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chown $user:$group $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o ! -group $group -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chown $user:$group \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o ! -group $group -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type d ! -perm -g+rwxs -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod g+rwxs $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type d ! -perm -g+rwxs -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chmod g+rwxs \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type d ! -perm -g+rwxs -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f ! -perm -g+rw -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod g+rw $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f ! -perm -g+rw -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chmod g+rw \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f ! -perm -g+rw -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -perm -o+w -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod o-w $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -perm -o+w -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chmod o-w \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat /usr/local/bin $JAVA_HOME/lib/security/cacerts -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -perm -o+w -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name "*.sh" ! -perm -g+x -print -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod +x $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name "*.sh" ! -perm -g+x -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chmod +x \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name \"*.sh\" ! -perm -g+x -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name "*.sh" ! -perm -u+x -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod +x $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name "*.sh" ! -perm -u+x -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chmod +x \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name \"*.sh\" ! -perm -u+x -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name "*.sh" ! -perm -o+x -print | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod +x $(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name "*.sh" ! -perm -o+x -print)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: ($0) chmod +x \$(find /home/$user /opt/grouper /opt/tier /opt/tier-support /opt/tomcat /home/tomcat -path /opt/grouper/slashRoot -prune -o -path /opt/grouper/logs -prune -o -type f -name \"*.sh\" ! -perm -o+x -print), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+chmod -f 660 /opt/grouper/certs/keys/*
+returnCode=$?
+echo "grouperDockerfile; INFO: (containerDockerfileInstall.sh) chmod 660 /opt/grouper/certs/keys/*, result: $returnCode"
+#dont fail since if no files then it exits with 1
+#if [ $returnCode != 0 ]; then exit $returnCode; fi
+
+lines=$(find /usr/local/bin -type f ! -perm -g+x | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod +x $(find /usr/local/bin -type f ! -perm -g+x)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /usr/local/bin -type f ! -perm -g+x), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+lines=$(find /usr/local/bin -type f ! -perm -o+x | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod +x $(find /usr/local/bin -type f ! -perm -o+x)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /usr/local/bin -type f ! -perm -o+x), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
+
+
+lines=$(find /usr/local/bin -type f ! -perm -u+x | wc -l)
+if [ $lines -ne 0 ]; then
+  chmod +x $(find /usr/local/bin -type f ! -perm -u+x)
+  returnCode=$?
+  echo "grouperDockerfile; INFO: (containerDockerfileInstallPermissions.sh) chmod +x \$(find /usr/local/bin -type f ! -perm -u+x), result: $returnCode"
+  if [ $returnCode != 0 ]; then exit $returnCode; fi
+fi
